a91bb804ebf17802ed750bd9d0842cd9cd17bb28
Jorandres13 <jaguerrero39@utpl.edu.ec>
2024-11-24 19:19:58 -0500

    xd

---
+++ JavaApplication9/src/Problema_MejorEgresado.java
@@ -0,0 +1,38 @@
+/* La UTPL, cada fin de periodo realiza las ceremonias de incorporación de sus
+nuevos profesionales de todas las carreras. En dicha ceremonia de entre todos
+los puntos desarrollados en la agenda, uno de los más relevantes es el
+discurso del mejor egresado de la promoción. Para elegirlo, las autoridades de
+entre algunos aspectos, considera su rendimiento académico durante toda la
+carrera, es aquí en donde la UTPL le contrata con el objetivo de colaborar en la
+automatización de este proceso de cálculo y generación de resultados
+académicos dados los siguientes lineamientos:
+
+- Autogenere los estudiantes con los datos/atributos necesarios (lo básico es
+su nombre y promedio final) con el fin de realizar la comparativa de su
+rendimiento académico entre todos los estudiantes a graduarse y así poder
+elegir al mejor egresado con el mayor promedio.
+
+- Realizar los cálculos y/o comparativas para filtrar al estudiante con el mayor
+promedio de egreso.
+
+- Al finalizar dicho proceso, como resultado Ud. debe mostrar los datos
+completos del mejor egresado, y el por qué fue elegido de entre todo el
+grupo.
+
+- Considerar la excepción/probabilidad de que puedan existir máximo 2
+mejores egresados
+ * @author Jorge Guerrero
+ */
+
+import java.util.Scanner;
+
+public class Problema_MejorEgresado {
+    public static void main(String[] args) {
+        Scanner tcl = new Scanner(System.in);
+        double nota = 0;
+        
+                
+        
+    }
+    
+}
